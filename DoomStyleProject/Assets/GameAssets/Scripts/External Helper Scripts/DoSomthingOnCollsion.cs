65d14c9d900522ba81ce3de17f6e9d0777996c04
Avi_156 <avi.g751@gmail.com>
2023-03-16 17:29:15 +0400

    first half of intro is conected

---
+++ DoomStyleProject/Assets/GameAssets/Scripts/External Helper Scripts/DoSomthingOnCollsion.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class DoSomthingOnCollsion : MonoBehaviour
+{
+    [SerializeField] bool isTrigger;
+    [SerializeField] bool onlyPlayOnce;
+    [SerializeField] string tag;
+    [SerializeField] UnityEvent DoSomethingOnCollisionOrTrigger;
+    bool played;
+   
+
+  
+
+    private void OnValidate()
+    {
+        GetComponent<Collider>().isTrigger = this.isTrigger;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag(tag))
+        PlayAnEventBasedOnCollisionType(!isTrigger);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(tag))
+         PlayAnEventBasedOnCollisionType(isTrigger);
+    }
+
+    public void PlayAnEventBasedOnCollisionType(bool collisionCondition)
+    {
+        if (onlyPlayOnce && played) return;
+
+        if (collisionCondition)
+        {
+            played = true;
+            DoSomethingOnCollisionOrTrigger?.Invoke();
+        }
+
+        
+    }
+}
